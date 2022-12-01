47a05878f88129d4bada5073904a2dfabd6bb2f4
201904 <201904@1220-005-22>
2022-12-01 09:13:17 +0300

    Добавьте файлы проекта.

---
+++ Fighting/Unit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fighting
+{
+    internal class Unit
+    {
+        public static int Strength {get; set;}
+        public static int Dexterity { get; set; }
+        public static int Intelligence { get; set; }
+        public static int Constitution { get; set; }
+        public static int Luck { get; set; }
+    }
+}
